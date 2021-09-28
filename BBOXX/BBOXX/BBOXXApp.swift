e8e13337f65b7458168aff0a5bc429aafce672ff
dev-Lena <keunnalee@gmail.com>
2021-09-28 22:34:43 +0900

    initial commit - create Xcode project for SwiftUI

---
+++ BBOXX/BBOXX/BBOXXApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBOXXApp.swift
+//  BBOXX
+//
+//  Created by Keunna Lee on 2021/09/28.
+//
+
+import SwiftUI
+
+@main
+struct BBOXXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
