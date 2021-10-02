9e1075db84cbb8f0c8793cb75863fd173da19956
dev-Lena <keunnalee@gmail.com>
2021-10-02 14:42:43 +0900

    add new xcode project named bboxx_iOS

---
+++ BBOXX_iOS/BBOXX_iOS/BBOXX_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBOXX_iOSApp.swift
+//  BBOXX_iOS
+//
+//  Created by Keunna Lee on 2021/10/02.
+//
+
+import SwiftUI
+
+@main
+struct BBOXX_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
