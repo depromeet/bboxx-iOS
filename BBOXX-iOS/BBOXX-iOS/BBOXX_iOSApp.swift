07a045eb614e214ea15ec5d08a95932f7f49343a
foreunwoo <agasiya99@gmail.com>
2021-10-02 16:14:56 +0900

    init new project

---
+++ BBOXX-iOS/BBOXX-iOS/BBOXX_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBOXX_iOSApp.swift
+//  BBOXX-iOS
+//
+//  Created by eunwoo on 2021/10/02.
+//
+
+import SwiftUI
+
+@main
+struct BBOXX_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBOXX-iOS/BBOXX-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BBOXX-iOS
+//
+//  Created by eunwoo on 2021/10/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
