347ac41d0fea2271b946a2724c39572db0015c90
dev-Lena <keunnalee@gmail.com>
2021-10-04 02:41:05 +0900

    add groups and basic files to build up clean architecture for SwiftUI

---
+++ BBOXX-iOS/BBOXX-iOS/AppState/AppState.swift
@@ -0,0 +1,12 @@
+//
+//  AppState.swift
+//  BBOXX-iOS
+//
+//  Created by Keunna Lee on 2021/10/04.
+//
+
+import Foundation
+
+struct AppState {
+    
+}
---
+++ BBOXX-iOS/BBOXX-iOS/UI/Scenes/LoginView.swift
@@ -0,0 +1,14 @@
+//
+//  LoginView.swift
+//  BBOXX-iOS
+//
+//  Created by Keunna Lee on 2021/10/04.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
