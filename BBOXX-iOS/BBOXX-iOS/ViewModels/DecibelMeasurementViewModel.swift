5b1535ebaee39eef5354adb23e8d3172249adcc4
foreunwoo <agasiya99@gmail.com>
2021-10-05 00:00:15 +0900

    [Create] DecibelMeasurementViewModel 생성

--- BBOXX-iOS/BBOXX-iOS/ViewModels/DecibelMeasurementViewModel.swift
+++ BBOXX-iOS/BBOXX-iOS/ViewModels/DecibelMeasurementViewModel.swift
@@ -1,10 +1,10 @@
 import Foundation
 import AVFoundation
 
-class MicrophoneMonitor: ObservableObject {
+class DecibelMeasurementViewModel: ObservableObject {
     
     // 1 녹음기와 타이머 선언
-    private var recorder: AVAudioRecorder
+    private var recorder: AVAudioRecorder?
     private var timer: Timer?
     
     // 2 데시벨과 최대 데시벨 변수 선언
@@ -18,10 +18,15 @@ class MicrophoneMonitor: ObservableObject {
         if audioSession.recordPermission != .granted {
             audioSession.requestRecordPermission { (isGranted) in
                 if !isGranted {
+                    // 추후 이 구간에 Alert 창을 띄워서 오디오 설정을 하고 돌아오게끔 유도
                     fatalError("You must allow audio recording for this demo to work")
                 }
             }
         }
+    }
+    
+    func initMonitoring() {
+        let audioSession = AVAudioSession.sharedInstance()
         
         // 4 실제로 오디오 녹음을 저장하지 않기 때문에 임시 디렉토리에 가까움, 오디오 설정
         let url = URL(fileURLWithPath: "/dev/null", isDirectory: true)
@@ -45,12 +50,12 @@ class MicrophoneMonitor: ObservableObject {
     
     // 6 초기화 되는 즉시 모니터링 시작
     private func startMonitoring() {
-        recorder.isMeteringEnabled = true
-        recorder.record()
+        recorder?.isMeteringEnabled = true
+        recorder?.record()
         timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { (timer) in
             // 7 오디오 전력 값 업데이트
-            self.recorder.updateMeters()
-            self.decibel = self.recorder.averagePower(forChannel: 0)
+            self.recorder?.updateMeters()
+            self.decibel = self.recorder?.averagePower(forChannel: 0) ?? 0
             
             if self.decibel > self.maxDecibel {
                 self.maxDecibel = self.decibel
@@ -58,9 +63,10 @@ class MicrophoneMonitor: ObservableObject {
         })
     }
     
-    // 8 초기화 해제될 때 리소스 정리
-    deinit {
+    // 8 측정 종료 시, 실행될 부분
+    func endMonitoring() {
         timer?.invalidate()
-        recorder.stop()
+        recorder?.stop()
     }
+    
 }
