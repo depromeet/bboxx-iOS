884242d2d7e42ecc26d0bcc0cb8c1bc91a3d15c2
foreunwoo <agasiya99@gmail.com>
2021-10-14 15:15:35 +0900

    fix spacing between views with padding

--- BBOXX-iOS/BBOXX-iOS/Views/Scenes/FeelingNoteView.swift
+++ BBOXX-iOS/BBOXX-iOS/Views/Scenes/FeelingNoteView.swift
@@ -11,47 +11,46 @@ struct FeelingNoteView: View {
             
             Spacer().frame(height: 20)
             
-            VStack {
-                HStack {
-                    Spacer().frame(width: 20)
-                    
-                    Text("21.09.17. 월")
-                        .font(.custom("HelveticaNeue", size: 16))
-                    
-                    Spacer()
-                    
-                    Button("다시쓰기", action: {
-                        
-                    })
-                    
-                    Spacer().frame(width: 17)
-                }
+            HStack {
+                Text("21.09.17. 월")
+                    .font(.custom("HelveticaNeue", size: 16))
+                    .padding(.leading, 20)
                 
-                Spacer().frame(height: 15)
+                Spacer()
                 
-                ScrollView() {
-                    HStack {
-                        Spacer().frame(width: 20)
-                        
-                        TextField("지금 네 감정은..", text: $title)
-                            .font(.custom("HelveticaNeue", size: 20))
-                            .lineLimit(2)
-                        
-                        Spacer().frame(width: 17)
-                    }
+                Button("다시쓰기", action: {
                     
-                    Spacer().frame(height: 10)
+                })
+                .padding(.trailing, 17)
+            }
+            
+            Spacer().frame(height: 15)
+            
+            ScrollView() {
+                TextField("지금 네 감정은..", text: $title)
+                    .font(.custom("HelveticaNeue", size: 20))
+                    .lineLimit(2)
                     
-                    HStack {
-                        Spacer().frame(width: 20)
-                        
-                        TextEditor(text: $content)
-                            .font(.custom("HelveticaNeue", size: 16))
-                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-                        
-                        Spacer().frame(width: 20)
-                    }
-                }
+                    .padding(.leading, 20)
+                    .padding(.trailing, 17)
+                
+                TextEditor(text: $content)
+                    .font(.custom("HelveticaNeue", size: 16))
+                    .frame(minWidth: 10, maxWidth: .infinity, minHeight: 200, maxHeight: .infinity)
+                    .border(Color.yellow)
+                    
+                    .padding(.top, 10)
+                    .padding(.leading, 20)
+                    .padding(.trailing, 20)
+            }
+            
+            Spacer().frame(height: 13)
+            
+            HStack {
+                Spacer()
+                
+                Text("0/500")
+                    .padding(.trailing, 21)
             }
             
             //            }
@@ -61,22 +60,20 @@ struct FeelingNoteView: View {
             //                    .stroke(Color.black, lineWidth: 1)
             //            )
             
-            HStack {
-                Spacer().frame(width: 25)
-                
-                Button("다 썼어!", action: {
-                    
-                })
-                .frame(maxWidth: .infinity, maxHeight: 54)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.black, lineWidth: 1)
-                )
-                
-                Spacer().frame(width: 25)
-            }
+            Spacer().frame(height: 21)
             
-            Spacer().frame(height: 20)
+            Button("다 썼어!", action: {
+                
+            })
+            .frame(maxWidth: .infinity, maxHeight: 54)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.black, lineWidth: 1)
+            )
+            
+            .padding(.leading, 25)
+            .padding(.trailing, 25)
+            .padding(.bottom, 20)
         }
     }
 }
