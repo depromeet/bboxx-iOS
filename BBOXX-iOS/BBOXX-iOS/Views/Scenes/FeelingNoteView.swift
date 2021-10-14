5b54d0c5979c4c8f1f4e420767fcb3892eec433a
foreunwoo <agasiya99@gmail.com>
2021-10-14 16:31:09 +0900

    convert current time

--- BBOXX-iOS/BBOXX-iOS/Views/Scenes/FeelingNoteView.swift
+++ BBOXX-iOS/BBOXX-iOS/Views/Scenes/FeelingNoteView.swift
@@ -1,26 +1,35 @@
 import SwiftUI
 
 struct FeelingNoteView: View {
+    
+    @ObservedObject var feelingNoteViewModel = FeelingNoteViewModel()
+    
     @State private var title: String = ""
     @State private var content: String = ""
     
     var body: some View {
         VStack {
             Text("지금 네 감정을 글로 써봐")
-                .font(.custom("HelveticaNeue", size: 24))
+                .font(.custom("HelveticaNeue", size: 20))
             
             Spacer().frame(height: 20)
             
             HStack {
-                Text("21.09.17. 월")
+                Text(self.feelingNoteViewModel.dateString)
                     .font(.custom("HelveticaNeue", size: 16))
                     .padding(.leading, 20)
                 
                 Spacer()
                 
-                Button("다시쓰기", action: {
+                Button(action: {
                     
-                })
+                }) {
+                    Image("redo")
+                        .frame(width: 16, height: 18)
+                    Text("다시쓰기")
+                }
+                .font(.custom("HelveticaNeue", size: 18))
+                
                 .padding(.trailing, 17)
             }
             
@@ -29,14 +38,13 @@ struct FeelingNoteView: View {
             ScrollView() {
                 TextField("지금 네 감정은..", text: $title)
                     .font(.custom("HelveticaNeue", size: 20))
-                    .lineLimit(2)
                     
                     .padding(.leading, 20)
-                    .padding(.trailing, 17)
+                    .padding(.trailing, 20)
                 
                 TextEditor(text: $content)
                     .font(.custom("HelveticaNeue", size: 16))
-                    .frame(minWidth: 10, maxWidth: .infinity, minHeight: 200, maxHeight: .infinity)
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 200, maxHeight: .infinity)
                     .border(Color.yellow)
                     
                     .padding(.top, 10)
@@ -49,11 +57,12 @@ struct FeelingNoteView: View {
             HStack {
                 Spacer()
                 
-                Text("0/500")
+                Text("0/1200")
+                    .font(.custom("HelveticaNeue", size: 12))
+                    
                     .padding(.trailing, 21)
             }
             
-            //            }
             //            .frame(maxWidth: .infinity)
             //            .overlay(
             //                RoundedRectangle(cornerRadius: 20)
@@ -62,7 +71,7 @@ struct FeelingNoteView: View {
             
             Spacer().frame(height: 21)
             
-            Button("다 썼어!", action: {
+            Button("다 썼어", action: {
                 
             })
             .frame(maxWidth: .infinity, maxHeight: 54)
