8ea0406c345ee08cee8d187366097ed45398fa53
foreunwoo <agasiya99@gmail.com>
2021-10-14 14:33:49 +0900

    update scrollview position

--- BBOXX-iOS/BBOXX-iOS/Views/Scenes/FeelingNoteView.swift
+++ BBOXX-iOS/BBOXX-iOS/Views/Scenes/FeelingNoteView.swift
@@ -11,66 +11,55 @@ struct FeelingNoteView: View {
             
             Spacer().frame(height: 20)
             
-            HStack {
-                Spacer().frame(width: 25)
+            VStack {
+                HStack {
+                    Spacer().frame(width: 20)
+                    
+                    Text("21.09.17. 월")
+                        .font(.custom("HelveticaNeue", size: 16))
+                    
+                    Spacer()
+                    
+                    Button("다시쓰기", action: {
+                        
+                    })
+                    
+                    Spacer().frame(width: 17)
+                }
+                
+                Spacer().frame(height: 15)
                 
                 ScrollView() {
-                    VStack {
-                        Spacer().frame(width: 16)
+                    HStack {
+                        Spacer().frame(width: 20)
                         
-                        HStack {
-                            Spacer().frame(width: 20)
-                           
-                            Text("21.09.17. 월")
-                                .font(.custom("HelveticaNeue", size: 16))
-                            
-                            Spacer()
-                            
-                            Button("다시쓰기", action: {
-                                
-                            })
-                            
-                            Spacer().frame(width: 17)
-                        }
+                        TextField("지금 네 감정은..", text: $title)
+                            .font(.custom("HelveticaNeue", size: 20))
+                            .lineLimit(2)
                         
-                        Spacer().frame(height: 15)
-                        
-                        HStack {
-                            Spacer().frame(width: 20)
-                            
-                            TextField("지금 네 감정은..", text: $title)
-                                .font(.custom("HelveticaNeue", size: 20))
-                                .lineLimit(2)
-                            
-                            Spacer().frame(width: 17)
-                        }
-
-                        Spacer().frame(height: 10)
-                        
-                        HStack {
-                            Spacer().frame(width: 20)
-                            
-                            TextEditor(text: $content)
-                                .font(.custom("HelveticaNeue", size: 16))
-                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-                            
-                            Spacer().frame(width: 20)
-                        }
-                        
-                        
+                        Spacer().frame(width: 17)
                     }
                     
+                    Spacer().frame(height: 10)
+                    
+                    HStack {
+                        Spacer().frame(width: 20)
+                        
+                        TextEditor(text: $content)
+                            .font(.custom("HelveticaNeue", size: 16))
+                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                        
+                        Spacer().frame(width: 20)
+                    }
                 }
-                .frame(maxWidth: .infinity)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(Color.black, lineWidth: 1)
-                )
-                
-                Spacer().frame(width: 25)
             }
             
-            Spacer().frame(height: 21)
+            //            }
+            //            .frame(maxWidth: .infinity)
+            //            .overlay(
+            //                RoundedRectangle(cornerRadius: 20)
+            //                    .stroke(Color.black, lineWidth: 1)
+            //            )
             
             HStack {
                 Spacer().frame(width: 25)
@@ -89,7 +78,6 @@ struct FeelingNoteView: View {
             
             Spacer().frame(height: 20)
         }
-        
     }
 }
 
