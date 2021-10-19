bcb3cd5cad641e4794dd1acd5eb2662a1f92146f
foreunwoo <agasiya99@gmail.com>
2021-10-19 17:16:05 +0900

    [Create] 셀 삭제 버튼 생성

--- BBOXX-iOS/BBOXX-iOS/Views/Scenes/NotificationCell.swift
+++ BBOXX-iOS/BBOXX-iOS/Views/Scenes/NotificationCell.swift
@@ -7,12 +7,23 @@ struct NotificationCell: View {
                 Text("2021. 10. 16.")
                     .font(.custom("HelveticaNeue", size: 14))
                 
+                    .padding(.top, 18)
+                
                 Text("1일 전")
                     .font(.custom("HelveticaNeue", size: 12))
                 
+                    .padding(.top, 18)
+                
                 Spacer()
+                
+                Button(action: {
+                    // 클릭 시 셀 삭제
+                }, label: {
+                    Text("X")
+                        .font(.custom("HelveticaNeue", size: 14))
+                })
+                .padding(.trailing, 10)
             }
-            .padding(.top, 18)
             .padding(.leading, 18)
             
             Text("자이언트펭귄! 한달 전(2021년 10월 20일)에 쓴 일기가 도착했어 📬 한번 읽어볼래?")
