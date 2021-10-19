4fe0880af6479c84b4e87d6c052c6000e43dbf56
foreunwoo <agasiya99@gmail.com>
2021-10-19 12:15:33 +0900

    [Create] 성장 일기 ui 배치

---
+++ BBOXX-iOS/BBOXX-iOS/ViewModels/GrowthNoteViewModel.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+class GrowthNoteViewModel: ObservableObject {
+    var dateString: String = ""
+    
+    init() {
+        convertCurrentDate()
+    }
+    
+    func convertCurrentDate() {
+        let nowDate = Date() // 현재의 Date
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateFormat = "MM. dd. EEEEE"
+        dateFormatter.locale = Locale(identifier:"ko_KR")
+        dateString = dateFormatter.string(from: nowDate)
+    }
+}
---
+++ BBOXX-iOS/BBOXX-iOS/Views/Scenes/GrowthNoteView.swift
@@ -0,0 +1,77 @@
+import SwiftUI
+
+struct GrowthNoteView: View {
+    
+    @ObservedObject var growthNoteViewModel = GrowthNoteViewModel()
+    
+    @State private var title: String = ""
+    @State private var content: String = ""
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("네 감정을 글로 담아봐")
+                .font(.custom("HelveticaNeue", size: 24))
+            
+                .padding(.top, 10)
+                .padding(.leading, 24)
+            
+            VStack {
+                HStack {
+                    Text(self.growthNoteViewModel.dateString)
+                        .font(.custom("HelveticaNeue", size: 12))
+                    
+                    Spacer()
+                    
+                    Text("\(content.count)/1200")
+                        .font(.custom("HelveticaNeue", size: 12))
+                }
+                .padding(.top, 30)
+                .padding(.leading, 24)
+                .padding(.trailing, 24)
+                
+                TextField("지금 내 감정은..", text: $title)
+                    .font(.custom("HelveticaNeue", size: 20))
+                    
+                    .padding(.top, 16)
+                    .padding(.leading, 24)
+                    .padding(.trailing, 56)
+                
+                Divider()
+                    .padding(.top, 10)
+                    .padding(.leading, 24)
+                    .padding(.trailing, 24)
+                
+                TextEditor(text: $content)
+                    .font(.custom("HelveticaNeue", size: 16))
+                    
+                    .padding(.top, 10)
+                    .padding(.leading, 24)
+                    .padding(.trailing, 31)
+                
+                Button("다 썼어", action: {
+                    
+                })
+                .frame(maxWidth: .infinity, maxHeight: 56)
+                .background(Color.yellow)
+                .cornerRadius(16)
+                
+                .padding(.top, 20)
+                .padding(.leading, 24)
+                .padding(.trailing, 24)
+                .padding(.bottom, 30)
+            }
+            .background(Color.white)
+            .cornerRadius(24, corners: [.topLeft, .topRight])
+            
+            .padding(.top, 20)
+            
+        }
+        .background(Color.yellow)
+    }
+}
+
+struct GrowthNoteView_Previews: PreviewProvider {
+    static var previews: some View {
+        GrowthNoteView()
+    }
+}
