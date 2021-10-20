3912256af6a30707508637453947a92dbacf2a5c
dev-Lena <keunnalee@gmail.com>
2021-10-21 01:14:33 +0900

    implement Sign In View (Login View)

--- BBOXX-iOS/BBOXX-iOS/Views/Scenes/Sign In/SignInView.swift
+++ BBOXX-iOS/BBOXX-iOS/Views/Scenes/Sign In/SignInView.swift
@@ -2,39 +2,70 @@ import SwiftUI
 import UIKit
 import AuthenticationServices
 
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView(viewModel: SignInViewModel())
+            .previewDevice("iPhone 12")
+    }
+}
+
+// TODO: manage the asset names("") with Enum
 struct SignInView: View {
     
     @ObservedObject var viewModel: SignInViewModel
     
-    var body: some View {
+    var body: some View{
+        
         VStack {
-            
-            Button("Sign in with Kakao") {
-                // TODO: add action
-            }
             Spacer()
+            Text(viewModel.commentFirstLine)
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+                .frame(maxWidth: .infinity, minHeight: 30, alignment: .center)
+            Text(viewModel.commentSecondLine)
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+                .frame(maxWidth: .infinity, minHeight: 30, alignment: .center)
+            Image(ImageAsset.BboxxIcon)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.horizontal,20)
+                .frame(maxHeight: 172, alignment: .center)
             
-            Button("Sign in with Google") {
-                // TODO: add action
+            // Buttons
+            VStack(spacing: 5){
+                
+                Button(action: {
+                    viewModel.attemptAppleSignIn()
+                }, label: {
+                    
+                    Image(ImageAsset.startWithAppleButton)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: .infinity, maxHeight: 54, alignment: .center)
+                })
+                
+                Button(action: {
+                    viewModel.attemptGoogleSignIn()
+                }, label: {
+                    
+                    Image(ImageAsset.startWithGoogleButton)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: .infinity,maxHeight: 54, alignment: .center)
+                })
+                
+                Button(action: {}, label: {
+                    Image(ImageAsset.startWithKakaoButton)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: .infinity,maxHeight: 54, alignment: .center)
+                })
             }
-            .buttonStyle(AuthenticationButtonStyle())
-            
+            .padding()
             Spacer()
-            AppleSignInButton().onTapGesture {
-                self.viewModel.attemptAppleSignIn()
-            }
         }
-    }
-}
-
-struct AuthenticationButtonStyle: ButtonStyle {
-    func makeBody(configuration: Self.Configuration) -> some View {
-        configuration.label
-            .foregroundColor(.white)
-            .padding()
-            .frame(maxWidth: .infinity)
-            .background(Color(.systemIndigo))
-            .cornerRadius(12)
-            .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color("BboxxBackgroundColor").ignoresSafeArea())
     }
 }
