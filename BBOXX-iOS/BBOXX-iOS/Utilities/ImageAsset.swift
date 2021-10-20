644cf31f3026d3c79e8c357beb0580cc1648c0f2
dev-Lena <keunnalee@gmail.com>
2021-10-21 01:09:42 +0900

    add properties in ImageAsset enum

---
+++ BBOXX-iOS/BBOXX-iOS/Utilities/ImageAsset.swift
@@ -0,0 +1,6 @@
+enum ImageAsset {
+    static let BboxxIcon = "BboxxIcon"
+    static let startWithAppleButton = "startWithAppleButton"
+    static let startWithGoogleButton = "startWithGoogleButton"
+    static let startWithKakaoButton = "startWithKakaoButton"
+}
