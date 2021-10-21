3ff31ab8052402747676cb64ece24b9928b93fa8
foreunwoo <agasiya99@gmail.com>
2021-10-21 11:21:57 +0900

    [Create] 디코딩을 위한 Response 모델 생성

--- BBOXX-iOS/BBOXX-iOS/Services/UserService.swift
+++ BBOXX-iOS/BBOXX-iOS/Services/UserService.swift
@@ -2,6 +2,10 @@ import Alamofire
 
 class UserService {
     
+    private init() {}
+        
+    static let shared = UserService()
+    
     func createNickname(){
         let url = APIConstants.BaseURL + "api/v1/generate-member-nickname"
         
@@ -12,7 +16,21 @@ class UserService {
         ]
         
         AF.request(url, method: .post, headers: header).responseJSON { (response) in
-            print("response: \(response)")
+            do {
+                let decoder = JSONDecoder()
+                
+                switch (response.result) {
+                case .success:
+                    let result = try decoder.decode(Response.self, from: response.data!)
+                    print("success: \(result)")
+                    break
+                case .failure(let error):
+                    print("errorCode: \(error._code)")
+                    print("errorDescription: \(error.errorDescription!)")
+                }
+            } catch let parsingError {
+                print("Error: ", parsingError)
+            }
         }.resume()
     }
 }
