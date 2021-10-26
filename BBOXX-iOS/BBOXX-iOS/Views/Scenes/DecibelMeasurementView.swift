18863c863b598137515f4456fc61d493dac2932b
foreunwoo <agasiya99@gmail.com>
2021-10-26 11:12:08 +0900

    [Create] 디자인 가이드대로 ui 재배치

--- BBOXX-iOS/BBOXX-iOS/Views/Scenes/DecibelMeasurementView.swift
+++ BBOXX-iOS/BBOXX-iOS/Views/Scenes/DecibelMeasurementView.swift
@@ -2,57 +2,46 @@ import SwiftUI
 
 struct DecibelMeasurementView: View {
     
-    @ObservedObject var decibelMeasurementViewModel = DecibelMeasurementViewModel()
+    @ObservedObject var viewModel = DecibelMeasurementViewModel()
     
     @State var showMic = true
         
     var body: some View {
         VStack {
-            Text(self.decibelMeasurementViewModel.guideString)
+            Text(self.viewModel.guideString)
                 .font(.custom("HelveticaNeue", size: 24))
+                .bold()
+                .foregroundColor(.white)
                 .multilineTextAlignment(.center)
                 
-            Spacer()
-                .frame(height: 65)
+//            Spacer()
+//                .frame(height: 46)
             
             ZStack {
-                Circle()
-                    .stroke(Color.black, lineWidth: 3)
-                    .frame(width: 172, height: 172)
-                
-                Circle()
-                    .trim(from: 0.0, to: self.decibelMeasurementViewModel.circleProgress)
-                    .stroke(Color.white, lineWidth: 3)
-                    .frame(width: 172, height: 172)
-                    .rotationEffect(Angle(degrees: -90))
-                
                 Button(action: {
                     self.startMeasuring()
                 }) {
                     if showMic {
-                        Image("mic.fill")
-                            .frame(width: 30, height: 32)
+                        Image(ImageAsset.micButton)
                     } else {
-                        Text("\(self.decibelMeasurementViewModel.timeLeft)")
-                        
+                        Image(self.viewModel.secondsImage)
+
                     }
                 }
-                .font(.custom("HelveticaNeue", size: 40))
-                .frame(width: 160, height: 160, alignment: .center)
-                .foregroundColor(.black)
-                .background(Color.yellow)
-                .cornerRadius(160)
-                
+                .frame(width: 165, height: 165, alignment: .center)
             }
             
-            Spacer()
-                .frame(height: 65)
+//            Spacer()
+//                .frame(height: 46)
         }
+
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black).ignoresSafeArea()
     }
     
     func startMeasuring() {
         self.showMic.toggle()
-        self.decibelMeasurementViewModel.setUpMonitoring()
+        self.viewModel.setUpMonitoring()
     }
 }
 
