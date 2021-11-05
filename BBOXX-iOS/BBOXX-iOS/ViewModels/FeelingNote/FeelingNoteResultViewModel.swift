b9c1f5cee31c9e4da7284d85b1293452dc4f823a
foreunwoo <agasiya99@gmail.com>
2021-11-05 13:23:00 +0900

    [Update] 닉네임 UserDefaults에 저장 후 사용, 뷰 레이아웃 조정

--- BBOXX-iOS/BBOXX-iOS/ViewModels/FeelingNote/FeelingNoteResultViewModel.swift
+++ BBOXX-iOS/BBOXX-iOS/ViewModels/FeelingNote/FeelingNoteResultViewModel.swift
@@ -1,5 +1,7 @@
 import Foundation
 
 class FeelingNoteResultViewModel: ObservableObject {
-    
+    var nickName: String {
+        return UserDefaults.standard.string(forKey: "nickname") ?? "사랑스러운딸기"
+    }
 }
