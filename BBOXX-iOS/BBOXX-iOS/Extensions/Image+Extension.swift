6f7d8142796716d2265ffbcc6b369aa9a1252284
dev-Lena <keunnalee@gmail.com>
2021-11-07 02:02:21 +0900

    feat: add Image extension

---
+++ BBOXX-iOS/BBOXX-iOS/Extensions/Image+Extension.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+extension Image {
+    func data(url: URL) -> Self {
+        if let data = try? Data(contentsOf: url) {
+            return Image(uiImage: UIImage(data: data)!)
+                .resizable()
+        }
+        return self.resizable()
+    }
+}
