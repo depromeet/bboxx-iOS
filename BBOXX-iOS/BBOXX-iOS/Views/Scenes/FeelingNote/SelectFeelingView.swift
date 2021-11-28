fb5804bad89d600845ad27e669c80c1cc6f3e8da
foreunwoo <agasiya99@gmail.com>
2021-11-28 14:23:05 +0900

    [Update] 감정 선택 화면 - 다크모드 시, 색상 반전되는 이슈 수정

--- BBOXX-iOS/BBOXX-iOS/Views/Scenes/FeelingNote/SelectFeelingView.swift
+++ BBOXX-iOS/BBOXX-iOS/Views/Scenes/FeelingNote/SelectFeelingView.swift
@@ -35,6 +35,7 @@ struct SelectFeelingView: View {
                 
                 Text("지금 네 감정을\n이 중에서 골라볼래?")
                     .font(.custom("Pretendard-Bold", size: 24))
+                    .foregroundColor(Color("BboxxGrayColor"))
                     
                     .padding(.top, 23)
                     .padding(.leading, 24)
